29382baef8e591f334beba5989b010ea9efe0af7
ChirajJ <chiraj.pep@gmail.com>
2018-01-29 09:56:27 +0530

    Using live stream

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var stream = require('stream');
+//var stream = require('stream');
 var EventEmitter =  require('events').EventEmitter;
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 var customEvent = new EventEmitter();
-
-var app = express();
 
 app.set('view engine', 'ejs');
 
@@ -19,21 +20,19 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
-var stream;
-
 app.get('/sdata', function(req, res){
     
     console.log('get Data');
-    stream  = fs.createReadStream('./files/small.file', {
+    const instream  = fs.createReadStream('./files/small.file', {
         flags: 'r',
         encoding: 'utf-8',
         fd: null,
         bufferSize: 1
     });
     var line = '';
-    //Right now it streams partially only
-    stream.on('data', function(chunk){
-        stream.pause();
+    //Right now it instreams partially only
+    instream.on('data', function(chunk){
+        instream.pause();
         console.log(chunk);
         res.send(chunk);
     });
@@ -42,18 +41,58 @@ app.get('/sdata', function(req, res){
 app.get('/hdata', function(req, res){
     
     console.log('get Data');
-    const stream  = fs.createReadStream('./files/huge.file', {
+    const instream  = fs.createReadStream('./files/huge.file', {
         flags: 'r',
         encoding: 'utf-8',
         fd: null,
         bufferSize: 1
     });
-    stream.pipe(res);
+    instream.pipe(res);
     
 });
 
 var port = process.env.PORT || 3000;
 
-app.listen(port, function(req, res){
+server.listen(port, function(req, res){
     console.log('Server served at port '+port);
+});
+
+io.sockets.on('connection', function(socket){
+
+    var query = socket.handshake.query;
+
+    console.log('Connection Established with type ', query.type);
+    
+    const instream  = fs.createReadStream('./files/'+query.type+'.file', {
+        flags: 'r',
+        encoding: 'utf-8',
+        fd: null,
+        bufferSize: 1
+    });
+    var line = '';
+
+    instream.on('data', function(chunk){
+        instream.pause();
+        console.log('---------------');
+        socket.emit('result', chunk);
+    });
+
+    socket.on('continue', function(){
+
+        console.log('**************');
+        console.log("Huge Stream...");
+        instream.resume();
+    });
+
+
+    instream.addListener('data', function(chunk){
+        instream.pause();
+        if(chunk == '\n'){
+            console.log(line);
+            line='';
+        }else{
+            line+=chunk;
+        }
+    });
+
 });
